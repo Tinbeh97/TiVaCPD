69606d66e1148481eabd17adc1fe721414358362
Jennifer Yu <jennyyu163@yahoo.ca>
2023-05-14 22:20:16 -0400

    modified peak detection

--- TiVaCPD/performance.py
+++ TiVaCPD/performance.py
@@ -162,22 +162,77 @@ class ComputeMetrics():
             warnings.filterwarnings("ignore")
             all_peak_prom = peak_prominences(distances, range(len(distances)))
         return all_peak_prom
-
+    
+    def fft_denoiser(self, x, n_components, to_real=True):
+        """Fast fourier transform denoiser.
+        
+        Denoises data using the fast fourier transform.
+        
+        Parameters
+        ----------
+        x : numpy.array
+            The data to denoise.
+        n_components : int
+            The value above which the coefficients will be kept.
+        to_real : bool, optional, default: True
+            Whether to remove the complex part (True) or not (False)
+            
+        Returns
+        -------
+        clean_data : numpy.array
+            The denoised data.
+            
+        References
+        ----------
+        .. [1] Steve Brunton - Denoising Data with FFT[Python]
+        https://www.youtube.com/watch?v=s2K1JfNR7Sc&ab_channel=SteveBrunton
+        
+        """
+        print(f'x: {x.shape}')
+        n = len(x)
+        
+        # compute the fft
+        fft = np.fft.fft(x, n)
+        
+        # compute power spectrum density
+        # squared magnitud of each fft coefficient
+        PSD = fft * np.conj(fft) / n
+        
+        # keep high frequencies
+        _mask = PSD > n_components
+        fft = _mask * fft
+        
+        # inverse fourier transform
+        clean_data = np.fft.ifft(fft)
+        
+        if to_real:
+            clean_data = clean_data.real
+        
+        return clean_data
+    
     def post_processing(self, score, threshold):
         
         if pd.isnull(score).all():
             score = np.zeros(shape=score.shape)
-
+        
         if not np.all((score==0)):
             
             score_peaks = np.zeros(shape = score.shape)
-            peaks = find_peaks_adaptive(score)
+            # score = self.fft_denoiser(score, 0.001)
+
+            # peaks = find_peaks_adaptive(score, window=self.moe * 2)
+            peaks = find_peaks(score, height=self.threshold, distance=self.moe * 2)[0]
 
             score_peaks[peaks] = 1
-            
+            # fig, ax = plt.subplots()
+            # ax.plot(score, label='score')
+            # ax.plot(score_peaks, label='score peaks')
+            # ax.legend()
+            # plt.show()
             return score_peaks 
         else:
             return score 
+        
 
 
 
